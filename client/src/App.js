afc4b4cd0e76cc6d264558a9dbe4a2d55a26ac55
Ariel Nunes da Silva <arielnds274@gmail.com>
2024-09-14 20:02:34 -0300

    feat: building the base of front end

---
+++ client/src/App.js
@@ -0,0 +1,129 @@
+import "./App.css"
+
+function App() {
+  return (<>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+  <main>
+      <header>
+        <a href="" className="logo">Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+
+    <div className="post">
+      <img src="https://i.scdn.co/image/ab67616d0000b2731df557c6310e8711a310790a"></img>
+      
+      <div className="text">
+        <h2>First impressions of OpenAI o1: An AI designed to overthink it</h2>
+
+        <p className="info">
+          <a className="author">BARBECUEBACONBURGUER</a>
+          <time>2024-09-14</time>
+        </p>
+
+        <p className="content">
+          Compared to GPT-4o, the o1 models feel like one step forward and two steps back.
+            OpenAI o1 excels at reasoning and answering complex questions, but the model is roughly four times more expensive to use than GPT-4o.
+            OpenAI’s latest model lacks the tools, multimodal capabilities, and speed that made GPT-4o so impressive.
+        </p>
+      </div>
+    </div>
+
+    <div className="post">
+      <img src="https://techcrunch.com/wp-content/uploads/2024/09/Screen-Shot-2024-09-13-at-6.40.22-PM.png?resize=1280,706"></img>
+      <div className="text">
+        <h2>First impressions of OpenAI o1: An AI designed to overthink it</h2>
+
+        <p className="info">
+          <a className="author">BARBECUEBACONBURGUER</a>
+          <time>2024-09-14</time>
+        </p>
+
+        <p className="content">
+          Compared to GPT-4o, the o1 models feel like one step forward and two steps back.
+            OpenAI o1 excels at reasoning and answering complex questions, but the model is roughly four times more expensive to use than GPT-4o.
+            OpenAI’s latest model lacks the tools, multimodal capabilities, and speed that made GPT-4o so impressive.
+        </p>
+      </div>
+    </div>
+
+    <div className="post">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR55MLgJzaw_c-zO10m69uK-9rn-IwL_c72Sg&s"></img>
+      <div className="text">
+        <h2>First impressions of OpenAI o1: An AI designed to overthink it</h2>
+
+        <p className="info">
+          <a className="author">BARBECUEBACONBURGUER</a>
+          <time>2024-09-14</time>
+        </p>
+
+        <p className="content">
+          Compared to GPT-4o, the o1 models feel like one step forward and two steps back.
+            OpenAI o1 excels at reasoning and answering complex questions, but the model is roughly four times more expensive to use than GPT-4o.
+            OpenAI’s latest model lacks the tools, multimodal capabilities, and speed that made GPT-4o so impressive.
+        </p>
+      </div>
+    </div>
+
+    <div className="post">
+      <img src="https://preview.redd.it/cite-1-banda-brasileira-que-se-parece-demais-com-uma-v0-mg4wh5kz2mod1.png?width=720&format=png&auto=webp&s=fe4a77e5f380ad9903430a74157744495189e235"></img>
+      <div className="text">
+        <h2>First impressions of OpenAI o1: An AI designed to overthink it</h2>
+
+        <p className="info">
+          <a className="author">BARBECUEBACONBURGUER</a>
+          <time>2024-09-14</time>
+        </p>
+
+        <p className="content">
+          Compared to GPT-4o, the o1 models feel like one step forward and two steps back.
+            OpenAI o1 excels at reasoning and answering complex questions, but the model is roughly four times more expensive to use than GPT-4o.
+            OpenAI’s latest model lacks the tools, multimodal capabilities, and speed that made GPT-4o so impressive.
+        </p>
+      </div>
+    </div>
+
+    <div className="post">
+      <img src="https://i.ytimg.com/vi/ogjJckYTFok/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCtnZIVPfy1sLPgcdAlUzXpj0eyDA"></img>
+      <div className="text">
+        <h2>First impressions of OpenAI o1: An AI designed to overthink it</h2>
+
+        <p className="info">
+          <a className="author">BARBECUEBACONBURGUER</a>
+          <time>2024-09-14</time>
+        </p>
+
+        <p className="content">
+          Compared to GPT-4o, the o1 models feel like one step forward and two steps back.
+            OpenAI o1 excels at reasoning and answering complex questions, but the model is roughly four times more expensive to use than GPT-4o.
+            OpenAI’s latest model lacks the tools, multimodal capabilities, and speed that made GPT-4o so impressive.
+        </p>
+      </div>
+    </div>
+
+    <div className="post">
+      <img src="https://i.ytimg.com/vi/KSeRN1XLr38/hqdefault.jpg?v=639b5200"></img>
+      <div className="text">
+        <h2>First impressions of OpenAI o1: An AI designed to overthink it</h2>
+
+        <p className="info">
+          <a className="author">BARBECUEBACONBURGUER</a>
+          <time>2024-09-14</time>
+        </p>
+
+        <p className="content">
+          Compared to GPT-4o, the o1 models feel like one step forward and two steps back.
+            OpenAI o1 excels at reasoning and answering complex questions, but the model is roughly four times more expensive to use than GPT-4o.
+            OpenAI’s latest model lacks the tools, multimodal capabilities, and speed that made GPT-4o so impressive.
+        </p>
+      </div>
+    </div>
+
+    </main>
+    </>
+  );
+}
+
+export default App;
