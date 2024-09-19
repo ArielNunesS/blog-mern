c820fa27887a2d8367553ca7971106a5de5b2aa6
Ariel Nunes da Silva <arielnds274@gmail.com>
2024-09-19 12:47:14 -0300

    feat: add mongoDB connection & implementing user registration funcionality

--- api/index.js
+++ api/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const User = require('./models/User');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.post('/register', (req, res) => {
-    res.json('test ok')
+const dataBase = mongoose.connect('mongodb+srv://blog:8QNdKVWchq3avphC@cluster0.odzhw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.create({
+        username,
+        password
+    });
+    res.json(userDoc);
 });
 
 app.listen(4000);
