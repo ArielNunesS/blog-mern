25aeaa626cdc5b39e3b86aec35a3da0a4ad20ffe
Ariel Nunes da Silva <arielnds274@gmail.com>
2024-09-24 15:36:39 -0300

    feat: implementing encryption for passwords

--- api/index.js
+++ api/index.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 const User = require('./models/User');
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
 
-const dataBase = mongoose.connect('mongodb+srv://blog:8QNdKVWchq3avphC@cluster0.odzhw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect('mongodb+srv://blog:8QNdKVWchq3avphC@cluster0.odzhw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
-    const userDoc = await User.create({
-        username,
-        password
-    });
-    res.json(userDoc);
+    try{
+        const userDoc = await User.create({
+            username,
+            password:bcrypt.hashSync(password,salt),
+        });
+        res.json(userDoc);
+    } catch (e){
+        console.log(e)
+        res.status(400).json(e)
+    }
 });
 
 app.listen(4000);
