2190a3f58e15a87d5beb3bdbe1cd77f9e84a1a17
Ariel Nunes da Silva <arielnds274@gmail.com>
2024-09-25 15:43:41 -0300

    implemented full user registration and login functionality

--- api/index.js
+++ api/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('./models/User');
 const app = express();
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'dm1893m89qjdasuijd189dj17dhaskjdh189'
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://blog:8QNdKVWchq3avphC@cluster0.odzhw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
@@ -23,6 +25,23 @@ app.post('/register', async (req, res) => {
     } catch (e){
         console.log(e)
         res.status(400).json(e)
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+
+    if(passOk){
+    // logged in
+        jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) =>{
+            if(err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+    } else{
+        res.status(400).json('wrong credentials');
     }
 });
 
