4137f5bcd7dd3350c0d3ade75ee01db8a19a8147
Ariel Nunes da Silva <arielnds274@gmail.com>
2025-03-11 17:01:55 -0300

    deploy

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header(){
   const { userInfo, setUserInfo } = useContext(UserContext);
 
     useEffect(() => {
-      fetch('https://blog-mern-backend-y37e.onrender.com/profile', {
+      fetch(`${process.env.REACT_APP_API_URL}/profile`, {
         credentials: 'include',
       })
         .then(response => { response.json()
@@ -46,20 +46,3 @@ export default function Header(){
       </header>
     </>);
 }
-
-// import { Link } from 'react-router-dom';
-
-// export default function Header() {
-//   return (
-//     <header>
-//       <Link to="/" className="logo">MeuBlog</Link>
-//       <nav>
-//         <Link to="/">Home</Link>
-//         <Link to="/categorias">Categorias</Link>
-//         <Link to="/sobre">Sobre</Link>
-//         <Link to="/login" className="btn-login">Entrar</Link>
-//         <Link to="/register" className="btn-register">Cadastrar</Link>
-//       </nav>
-//     </header>
-//   );
-// }
