810ef8b8854a52c69f4f329a2e1dc63d58df796f
Ariel Nunes da Silva <arielnds274@gmail.com>
2025-03-11 17:18:58 -0300

    fix: cors erros

--- api/index.js
+++ api/index.js
@@ -16,8 +16,8 @@ const secret = process.env.JWT_SECRET || 'dm1893m89qjdasuijd189dj17dhaskjdh189';
 
 app.use(cors({
     credentials:true,
-    origin:'process.env.FRONTEND_URL' || 'http://localhost:3000'
-    }));
+    origin:'https://blog-mern-frontend-agxuyfsw0-ariel-nunes-da-silvas-projects.vercel.app/' || 'http://localhost:3000',
+}));
 
 app.use(express.json());
 app.use(cookieParser());
