218242cbe91d4da3f903422ab96e92bdaf4b34bd
Ariel Nunes da Silva <arielnds274@gmail.com>
2025-03-11 16:09:57 -0300

    .

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,7 +5,7 @@ export default function HomePage(){
     const [ posts, setPosts ] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:4000/posts').then(response => {
+        fetch(process.env.PORT).then(response => {
             response.json().then(posts => {
                 setPosts(posts);
             });
