76dba9c06455005e5f2f8a27ef8e0e194c252c7b
Ariel Nunes da Silva <arielnds274@gmail.com>
2025-03-11 23:58:30 -0300

    .

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ const secret = process.env.JWT_SECRET || 'dm1893m89qjdasuijd189dj17dhaskjdh189';
 
 app.use(cors({
     credentials:true,
-    origin:'https://blog-mern-frontend-beta.vercel.app/' || 'http://localhost:3000',
+    origin:'https://blog-mern-frontend-beta.vercel.app' || 'http://localhost:3000',
 }));
 
 app.use(express.json());
