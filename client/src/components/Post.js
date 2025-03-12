e682ca6388ea7222b00d7abcae9cfcadccec0307
Ariel Nunes da Silva <arielnds274@gmail.com>
2025-03-12 17:52:19 -0300

    fix: img src

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -31,7 +31,7 @@ export default function Post({_id, title, summary, content, cover, createdAt, au
     <div className="post-card">
         <div className="post-image">
             <Link to={`/posts/${_id}`}>
-                <img src={'http://localhost:4000/'+cover}></img>
+                <img src={process.env.UPLOADS+cover}></img>
             </Link>
         </div>
 
