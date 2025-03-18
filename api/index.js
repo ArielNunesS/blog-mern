6e1ece2ceb6868b23058cc76bb749a9621d40194
Ariel Nunes da Silva <arielnds274@gmail.com>
2025-03-18 20:20:16 -0300

    fix: cors error

--- api/index.js
+++ api/index.js
@@ -18,6 +18,19 @@ const allowedOrigins = [
     'http://localhost:3000'
 ];
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://blog-mern-frontend-beta.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+    
+    next();
+  });
+
 app.use(cors({
     credentials:true,
     origin: function(origin, callback) {
