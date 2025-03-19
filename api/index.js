e6ade45351738e0710b8ec51076c20a3f029436d
Ariel Nunes da Silva <arielnds274@gmail.com>
2025-03-19 19:51:21 -0300

    fix: cors

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ const secret = process.env.JWT_SECRET || 'dm1893m89qjdasuijd189dj17dhaskjdh189';
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://blog-mern-frontend-beta.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
     if (req.method === 'OPTIONS') {
