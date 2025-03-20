9d71b9d2c6fe182c974c7827268b0f31c374b9b1
Ariel Nunes da Silva <arielnds274@gmail.com>
2025-03-20 17:56:47 -0300

    .

--- api/index.js
+++ api/index.js
@@ -112,7 +112,7 @@ app.post('/posts', uploadMiddleware.single('file'), async (req, res) => {
         if (err) throw err;
         const {title, summary, content} = req.body;
 
-        const imageUrl = `/uploads/${newPath.split('/').pop()}`;
+        const imageUrl = `uploads/${newPath.split('/').pop()}`;
 
         const postDoc = await Post.create({
             title,
