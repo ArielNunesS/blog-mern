b2dff06292408d42b707e7f935e545b5c7b0b06f
Ariel Nunes da Silva <arielnds274@gmail.com>
2025-03-20 16:59:14 -0300

    .

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -31,7 +31,7 @@ export default function Post({_id, title, summary, content, cover, createdAt, au
     <div className="post-card">
         <div className="post-image">
             <Link to={`/posts/${_id}`}>
-                <img src={`${process.env.REACT_APP_API_URL}/uploads/${cover}`} alt='Post img'/>
+                <img src={`${process.env.REACT_APP_API_URL}${cover}`} alt='Post img'/>
             </Link>
         </div>
 
